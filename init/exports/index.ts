ed2e39a01af3b0dc38f6e72924e3bbece3ae4fce
Chris V <cvaldezchavez@gmail.com>
2025-08-22 02:03:21 -0500

    behavior tsc

---
+++ init/exports/index.ts
@@ -0,0 +1,8 @@
+import { Hero } from './classes/Hero';
+import { Power } from './data/powers';
+
+const ironman = new Hero('Ironman', 1, 55);
+
+console.log( ironman );
+
+console.log( powers );
---
+++ init/generics/index.ts
@@ -0,0 +1,12 @@
+import { printObject, genericFunction, genericFunctionArrow } from './generics'
+
+/*
+console.log( 123 );
+console.log( 'Hello word');
+console.log( new Date() );
+*/
+
+const name = 'chris';
+console.log( genericFunction(3.1416).toFixed(2) );
+console.log( genericFunction( name.toUpperCase() );
+console.log( genericFunctionArrow( 'TTILE' ).toLowerCase() );
