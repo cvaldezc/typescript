e55b1d9209f66f3793fb54cf0dd2b6b8576d45da
Chris V <cvaldezchavez@gmail.com>
2025-08-23 02:07:40 -0500

    add express project

---
+++ init/project/src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.status(200);
+    res.json( { "id": "123"} );
+});
+
+app.listen(port, () => {
+    console.log( `App listening on port: ${port}`)
+});
+
